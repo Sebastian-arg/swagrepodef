c8265a7ec2b34bf2e89e753d6789231dcc959c69
Santiago Nicolas Ormazabal <santiagonicolasormazabal@gmail.com>
2025-10-29 23:09:49 -0300

    eventos conectado

--- mi-agenda/src/app/auth.interceptor.ts
+++ mi-agenda/src/app/auth.interceptor.ts
@@ -1,7 +1,23 @@
+// src/app/auth.interceptor.ts
+
 import { HttpInterceptorFn } from '@angular/common/http';
+// 👈 Importa las funciones y tokens necesarios para SSR
+import { PLATFORM_ID, inject } from '@angular/core';
+import { isPlatformBrowser } from '@angular/common'; 
 
 export const authInterceptor: HttpInterceptorFn = (req, next) => {
-    const authToken = localStorage.getItem('user_token'); 
+    // 1. Inyecta el token PLATFORM_ID
+    const platformId = inject(PLATFORM_ID);
+    
+    // 2. Declara authToken fuera del bloque condicional
+    let authToken: string | null = null;
+
+    // 3. 🛡️ PROTECCIÓN SSR: Solo accede a localStorage si es el navegador
+    if (isPlatformBrowser(platformId)) {
+        authToken = localStorage.getItem('user_token'); 
+    }
+    
+    // El resto de la lógica permanece igual
     if (authToken) {
         const cloned = req.clone({
             headers: req.headers.set('Authorization', `Bearer ${authToken}`)
